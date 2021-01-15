9d46c2ba881239d8164d4d01d6a7ff4c9cec060d
tengosc <aleksander.atamanczuk@gmail.com>
2021-01-15 13:41:50 +0100

    feature: create question panel view and display content form API

--- src/main.js
+++ src/main.js
@@ -1,25 +1,3 @@
-import GOTPanel from "./got-panel";
-import RMPanel from "./rm-panel";
-import BBPanel from "./bb-panel";
+import { showQuestion } from "./quiz-panel";
 
-const gotQuizPanel = new GOTPanel(53);
-const rmQuizPanel = new RMPanel(20);
-const bbQuizPanel = new BBPanel(50);
-
-console.log("GoT");
-let question = gotQuizPanel.drawAnswers(1);
-console.log(question);
-let newOrder = gotQuizPanel.shuffledArray(question);
-console.log(newOrder);
-
-console.log("R&M");
-question = rmQuizPanel.drawAnswers(1);
-console.log(question);
-newOrder = rmQuizPanel.shuffledArray(question);
-console.log(newOrder);
-
-console.log("BB");
-question = bbQuizPanel.drawAnswers(1);
-console.log(question);
-newOrder = bbQuizPanel.shuffledArray(question);
-console.log(newOrder);
+showQuestion();
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,36 @@ module.exports = {
     path: path.resolve(__dirname),
     filename: "index.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+        ],
+      },
+    ],
+  },
 };
